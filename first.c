71c71b76bc3a72778b0c666d342d40ed44a439f5
mstsurnalyakter <surnalyms@gmail.com>
2024-06-17 14:56:32 +0600

    added new code

---
+++ first.c
@@ -0,0 +1,18 @@
+# include<stdio.h>
+
+int main(){
+
+
+int a,b,division;
+
+printf("Enter a =");
+scanf("%d",&a);
+
+printf("Enter b =");
+scanf("%d",&b);
+
+division = a/b;
+printf("division of = %d",division);
+
+    return 0;
+}
