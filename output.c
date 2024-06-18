f67b371a6182c2970228c68ea429a541394457db
mstsurnalyakter <surnalyms@gmail.com>
2024-06-18 07:41:08 +0600

    output

---
+++ output.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    /* code */
+    printf("Hello World");
+    int n=4;
+    float pi = 3.14;
+    char star ='@';
+
+
+    printf("%d \n",n);
+    printf("%f \n",pi);
+    printf("%c \n",star);
+    return 0;
+}
