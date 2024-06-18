fbac381cf75a3176d872ccd7468dc10ddee7d339
mstsurnalyakter <surnalyms@gmail.com>
2024-06-18 09:29:15 +0600

    control operator

---
+++ controll.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+    int age;
+    printf("enter age: ");
+    scanf("%d",&age);
+
+    if (age > 20)
+    {
+        printf("adult \n");
+        printf("You can vote \n");
+        printf("You can drive \n");
+
+        /* code */
+    }else
+    {
+        /* code */
+        printf("not adult \n");
+    }
+
+printf("Thank you");
+
+    return 0;
+}
